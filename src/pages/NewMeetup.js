901c9fc593882b0843d50859865f2fe27e6b31a0
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-05-15 22:18:55 +0200

    added HTTP POST request to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,23 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-4049b-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
   console.log("hello");
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <p>NewMeetupPage</p>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
